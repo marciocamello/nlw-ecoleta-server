e224ba8ee3c088cd9a5cd23b6d9c32f21c20c0e7
Marcio Camello <marciocamello@outlook.com>
2020-06-02 13:04:36 -0300

    Add routes to points and items, add cors and separete routes with controllers

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,10 @@ import path from 'path'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
